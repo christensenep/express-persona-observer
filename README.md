b03c6a384278c5192f30a707e2ac885c83f29a64
Mike Larsson <mikelarssonftw@gmail.com>
2013-12-20 11:59:44 -0500

    Early spike

---
+++ README.md
@@ -0,0 +1,72 @@
+# express-persona-observer
+
+Opinionated Mozilla Persona integration for Express. express-persona-observer adds functionality
+to [express-persona][] to make integration of Persona using its Observer API even more seamless.
+
+[express-persona]: https://github.com/jbuck/express-persona.git
+
+## Quick start
+Install using npm: `npm install express-persona-observer`
+
+Include the module inside your Express application:
+
+```javascript
+var express = require("express");
+var persona = require("express-persona-observer");
+var app = express();
+
+app.use(express.json());
+app.use(express.urlencoded());
+app.use(express.cookieParser());
+app.use(express.session({
+  secret: "mozillapersonaiswatchingyou"
+}));
+
+persona.express(app, {
+  audience: "http://localhost:8888" // Must match your browser's address bar
+});
+```
+
+Include the Persona library and login script in your web pages:
+
+```html
+<script src="https://login.persona.org/include.js"></script>
+<script src="/persona/login.js"></script>
+```
+
+Add login and logout buttons to your page:
+
+```html
+<button id="login">Log In</button>
+<button id="logout">Log Out</button>
+```
+
+Like [express-persona], by default the user's email address is added to `req.session.email`
+when their email is validated.
+
+You can view and run a complete example in the [examples directory](#).
+
+## Documentation
+
+`express-persona-observer` provides both the server and client-side code to integrate Persona
+into your express application. It provides several useful route middleware methods, request
+methods, and application locals to take the pain out of writing Persona-based applications.
+
+### API
+
+* `express(app, options)`
+  * `express` is an instance of the express server that you want to add routes to.
+  * `options` is an object. It has one required parameter, `audience`.
+
+### Required options
+
+* `audience` = The URL of your express app when viewed in a browser. Must include the protocol, hostname, and port.
+  * Example: `http://example.org:80`, `https://example.org:443`
+
+### Optional options
+
+* `express-persona-observer` supports all [`express-persona` options](https://github.com/jbuck/express-persona/tree/v0.1.0#optional-options).
+* `syncResponse(req, res, next)` - Function to generate response when the appliation wants to sync it's session with Persona
+  * Default: none
+  * `req, res, next` are the typical express middleware callback arguments
+* **TODO**: finish docs
---
+++ index.js
@@ -0,0 +1,85 @@
+var fs = require('fs');
+var path = require('path');
+var expressPersona = require('express-persona');
+var minstache = require('minstache');
+
+var loginjs = minstache.compile(fs.readFileSync(path.join(__dirname, './templates/login.js'), {encoding: 'utf-8'}));
+
+var redirects;
+
+module.exports.ensureLoggedIn = function ensureLoggedIn (req, res, next) {
+  if (!req.fromLoggedInUser()) {
+    if (redirects.notLoggedIn)
+      return res.redirect(303, redirects.notLoggedIn);
+    else
+      return next('Not logged in');
+  }
+  return next();
+};
+
+module.exports.ensureLoggedOut = function ensureLoggedOut (req, res, next) {
+  if (req.fromLoggedInUser()) {
+    if (redirects.notLoggedOut)
+      return res.redirect(303, redirects.notLoggedOut);
+    else
+      return next('Logged in');
+  }
+  return next();
+};
+
+module.exports.express = function (app, config) {
+  config.logoutPath = config.logoutPath || "/persona/logout";
+  config.verifyPath = config.verifyPath || "/persona/verify";
+  config.loginjsPath = config.loginjsPath || "/persona/login.js";
+  config.sessionKey = config.sessionKey || "email";
+
+  redirects = config.redirects || {};
+
+  function exemptPath (url) {
+    return url === config.logoutPath
+        || url === config.verifyPath
+        || url === config.loginjsPath;
+  }
+
+  app.use(function (req, res, next) {
+    // use req.fromLoggedInUser() in route handlers to determine if you have a user or not
+    req.fromLoggedInUser = function () {
+      return req.session && req.session[config.sessionKey];
+    };
+    // email address of the logged in user, or null, for use in templates
+    res.locals = {
+      loggedInUser: req.session[config.sessionKey] || null,
+      loginScriptUrl: config.loginjsPath
+    };
+    next();
+  });
+
+  if (config.syncResponse) {
+    app.use(function syncWithPersona (req, res, next) {
+      if (exemptPath(req.originalUrl))
+        return next();
+      if (req.session[config.sessionKey] !== undefined)      // undefined indicates unknown, whereas null indicates logged out
+        return next();
+      return config.syncResponse(req, res, next);
+    });
+  }
+
+  app.get(config.loginjsPath, function (req, res, next) {
+    var email = undefined;
+    if (req.session && req.session[config.sessionKey]) 
+      email = '"' + req.session.email + '"';
+    else if (req.session[config.sessionKey] === null)
+      email = null;
+
+    res.type('.js');
+    return res.send(loginjs({
+      loggedInUser: email,
+      verifyPath: config.verifyPath,
+      logoutPath: config.logoutPath,
+      loginSelector: '#login',
+      logoutSelector: '#logout'
+    }));
+  });
+
+  expressPersona(app, config);
+}
