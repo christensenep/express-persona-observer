5a6d97f3cb5446e19bd86a78820a0054ffc913b6
Mike Larsson <mikelarssonftw@gmail.com>
2014-01-07 11:44:25 -0500

    Break out test files and test locals

--- test/app.js
+++ test/app.js
@@ -1,7 +1,9 @@
 var express = require('express');
+var http = require('http');
 var persona = require('..');
 var request = require('request');
 var _ = require('underscore');
+var assert = require('assert');
 var url = require('url');
 
 function runTestServer (opts, done) {
@@ -39,6 +41,9 @@ function runTestServer (opts, done) {
   app.get('/loggedOutOnly', persona.ensureLoggedOut, function (req, res, next) {
     return res.send('OK'); 
   });
+  app.get('/locals', function (req, res, next) {
+    return res.json(res.locals);
+  });
 
   app.use(function (err, req, res, next) {
     return res.json(500, {
@@ -47,15 +52,16 @@ function runTestServer (opts, done) {
     });
   });
 
-  app.listen(port, function () {
-    var server = this;
-
-    server.urlTo = function (path) {
-      return url.resolve('http://localhost:' + port, path);
-    };
-
-    done(server);
+  var server = http.createServer(app);
+  server.urlTo = function (path) {
+    return url.resolve('http://localhost:' + port, path);
+  };
+  server.on('error', function (err) {
+    /* Let's not have one test failure take down the suite by hogging the port */
+    opts.port = ++port;
+    runTestServer(opts, done);
   });
+  server.listen(port, done.bind(this, server));
 }
 
 /* Like should.js it() but takes optional opts argument and runs test app
@@ -69,9 +75,17 @@ module.exports = function app (name, opts, cb) {
 
   it(name, function (done) {
     runTestServer(opts, function (server) {
+      var req = request.defaults({jar: request.jar()});
       cb.call({ 
         server: server,
-        request: request.defaults({jar: request.jar()})
+        request: req,
+        login: function (email, cb) {
+          req({
+            method: 'POST',
+            url: server.urlTo('/login'),
+            json: { email: email }
+          }, cb);
+        }
       }, function () {
         server.close();
         done();
